40f71d06d2ba6505880f186e7e1ed296e3bdbefd
sinclairr <rickysinclair@gmail.com>
2019-06-26 14:56:54 +1000

    26/6/19 - v0.4

--- sql_basics.py
+++ sql_basics.py
@@ -12,12 +12,20 @@ conn.execute('''CREATE TABLE CUSTOMERS
 
 myTuple = """INSERT INTO CUSTOMERS (id, name, age, DOB) VALUES (?,?,?,?)""";
 
-records_to_insert = [(2, 'Jon', 27, '2018-01-11'),
-                     (3, 'Jane', 28, '2017-12-11'),
-                     (4, 'Bill', 35, '2018-03-23'),
-                     (5, 'Jon', 42, '2018-01-11'),
-                     (6, 'Jane', 21, '2017-12-11')]
+records_to_insert = [[2, 'Jon', 27, '2018-01-11'],
+                     [3, 'Jane', 28, '2017-12-11'],
+                     [4, 'Bill', 35, '2018-03-23'],
+                     [5, 'Jon', 42, '2018-01-11'],
+                     [6, 'Jane', 21, '2017-12-11']]
 
 conn.executemany(myTuple, records_to_insert)
+
+record = conn.execute("SELECT id, name, age, DOB FROM CUSTOMERS")
+
+for row in record:
+    print("ID = ", row[0])
+    print("NAME = ", row[1])
+    print("AGE = ", row[2])
+    print("DOB = ", row[3], '\n')
 
 conn.close()
